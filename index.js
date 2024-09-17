969e8ed02e04a89051826ff60ce7436d69da222f
Borec191 <115628041+JanBenedikt@users.noreply.github.com>
2024-09-17 11:50:56 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get("/random/" , (req , res) => {
+  let random = Math.random();
+  res.json({number: random});
+})
+
+app.get("/random/:max" , (req , res) => {
+  let isLucky = req.query.lucky;
+  if  (isLucky !=0 || isLucky !=1){
+    res.status(400).send("status needs to be 0 or 1")
+  } 
+  let random = Math.ceil(Math.random() * req.params.max);
+  if (isLucky){
+    let LuckyRandom = Math.ceil(Math.random() * req.params.max);
+    random = Math.max(random, luckyRandom);
+  }
+  res.json({number: random});
+})
